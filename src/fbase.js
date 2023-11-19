d88c338607cc551c401101aafe9b48ad05ae609e
dasdfadssda <quddkflty@gmail.com>
2023-11-19 17:13:11 +0900

    수정

---
+++ src/fbase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const dbService = getFirestore(app); 
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { app, dbService, analytics, auth, storage };
