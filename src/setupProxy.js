caf97da5a69132d093081655d72f273e5a3971e6
dasdfadssda <quddkflty@gmail.com>
2023-12-04 14:22:51 +0900

    수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware({
-      target: "https://openapi.naver.com",
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware({
+//       target: "https://openapi.naver.com",
+//       changeOrigin: true,
+//     })
+//   );
+// };
