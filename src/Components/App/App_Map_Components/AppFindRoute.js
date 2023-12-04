54871e6e16425c59a665ac3c1c4145e8fc7690c1
dasdfadssda <quddkflty@gmail.com>
2023-12-04 23:26:53 +0900

    길찾기 시작

--- src/Components/App/App_Map_Components/AppFindRoute.js
+++ src/Components/App/App_Map_Components/AppFindRoute.js
@@ -1,44 +1,37 @@
 import React, { useState, useRef } from "react";
-import { NaverMap, Polyline, Marker } from "react-naver-maps";
+import {
+  Container as MapDiv,
+  NaverMap,
+  useNavermaps,
+  Overlay,
+  Marker,
+  useMap,
+  useListener,
+} from "react-naver-maps";
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../../../Style/theme";
+
 
 const AppFindRoute = () => {
-  const [center, setCenter] = useState({ lat: 37.5665, lng: 126.978 });
-  const zoom = 10;
-  const departureRef = useRef(null);
-  const arrivalRef = useRef(null);
+  const [naverMap, setNaverMap] = useState();
+  const NAVER_API_KEY = process.env.REACT_APP_NAVER_MAP_API_KEY;
+  const NAVER_ID = process.env.REACT_APP_NAVER_ID;
+  const navermaps = window.naver.maps;
 
-  const routes = {
-    trafast: {
-      path: [[126.978, 37.5665]],
-    },
-  };
-  return (
-    <NaverMap
-      //ncpClientId={process.env.REACT_APP_NAVER_CLOUD_CLIENT_ID}
-      style={{
-        width: "100%",
-        height: "100vh",
-      }}
-      zoom={zoom}
-      center={center}
-      onCenterChanged={(newCenter) => setCenter(newCenter)}
-      onZoomChanged={(newZoom) => console.log(newZoom)}
-    >
-      {routes && routes.trafast && (
-        <Polyline
-          path={routes.trafast.path.map(
-            ([lng, lat]) => new window.naver.maps.LatLng(lat, lng)
-          )}
-          strokeColor={"#ff3344"}
-          strokeStyle={"solid"}
-          strokeOpacity={0.8}
-          strokeWeight={4}
-        />
-      )}
-      {departureRef.current && <Marker position={departureRef.current} />}
-      {arrivalRef.current && <Marker position={arrivalRef.current} />}
-    </NaverMap>
-  );
+  return <ThemeProvider theme={theme}>
+    <MapDiv
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "730px",
+          display: "flex",
+          backgroundColor: "#fff",
+          alignItems: "center",
+          overflow: "hidden",
+        }}
+        onInitialized={(map) => setNaverMap(map)}
+      ></MapDiv>
+  </ThemeProvider>;
 };
 
 export default AppFindRoute;
